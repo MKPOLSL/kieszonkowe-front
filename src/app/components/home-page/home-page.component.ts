76083ca02fdd728f857e0201a4b123b34e4b886b
Mateusz Kulinowski <mat.kulinowski@gmail.com>
2020-10-25 22:45:25 +0100

    first GET

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { PocketMoneyService } from 'app/services/pocket-money.service';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
-  
-  constructor() { }
+  data: string[] = [];
+  constructor(private service: PocketMoneyService) {}
 
   ngOnInit(): void {
+    this.service.getAll().subscribe((data) => (this.data = data));
   }
-
 }
