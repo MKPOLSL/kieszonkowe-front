fae5e0e79943defe9f3ea7e52f9475b6a15c63f3
paweryk807 <paweryk807@student.polsl.pl>
2021-02-04 12:16:05 +0100

    changes in css, radio buttons wip

--- src/app/components/statistics/statistics.component.ts
+++ src/app/components/statistics/statistics.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Education } from '@app/_models/education';
-import { Region } from '@app/_models/region';
 import { StatisticsService } from 'app/services/statistics.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AlertService } from '@app/services/alert.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { first } from 'rxjs/operators';
 import { Statistics } from '@app/_models/statistics';
-import { Chart} from 'chart.js'
+import { Chart } from 'chart.js'
+//import {MatRadioModule} from '@angular/material/radio';
 
 @Component({
   selector: 'app-statistics',
@@ -16,15 +15,19 @@ import { Chart} from 'chart.js'
 })
 
 export class StatisticsComponent implements OnInit {
-    loading = false;
-    submitted = false;
-    returnUrl: string;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
 
-    regions = null;
-    educations = null;
-    selectedRegion = null;
-    selectedEducation = null;
-    statistics : Statistics = null;
+  regions = null;
+  educations = null;
+  selectedRegion = null;
+  selectedEducation = null;
+  statistics: Statistics = null;
+
+  regionSelector : string;
+  regionOptions : string[] = ['Miasta', 'Województwa'];
+
 
   constructor(
     private formBuilder: FormBuilder,
@@ -33,7 +36,7 @@ export class StatisticsComponent implements OnInit {
     private router: Router,
     private route: ActivatedRoute
 
-  ) {}
+  ) { }
 
   get f() { return this.form.controls; }
 
@@ -63,53 +66,53 @@ export class StatisticsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    
+
     var myChart = new Chart("myChart", {
       type: 'bar',
       data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-          datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
       },
       options: {
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero: true
-                  }
-              }]
-          }
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
       }
     });
 
     this.statisticsService
-    .getEducations()
-    .pipe(first())
-    .subscribe(educations => (this.educations = educations));
+      .getEducations()
+      .pipe(first())
+      .subscribe(educations => (this.educations = educations));
 
     this.statisticsService
-    .getRegions()
-    .pipe(first())
-    .subscribe(regions => (this.regions = regions));
+      .getRegions()
+      .pipe(first())
+      .subscribe(regions => (this.regions = regions));
   }
 
   onSubmit() {
@@ -122,24 +125,24 @@ export class StatisticsComponent implements OnInit {
 
     // stop here if form is invalid
     if (this.form.invalid) {
-        return;
+      return;
     }
 
     this.loading = true;
 
     this.statisticsService.sendRegionAndEducation(this.f.education.value, this.f.region.value)
-        .pipe(first())
-        .subscribe(
-            data => {               
-                if(data != null) {
-                  this.statistics = Object.assign(new Statistics(), data);
-                  this.statistics.standardDeviationAmount = Math.round((this.statistics.standardDeviationAmount + Number.EPSILON) * 100) / 100
-                  this.router.navigate([this.returnUrl]);
-                }
-            },
-            error => {
-                this.alertService.error(error);
-                this.loading = false;
-            });
-}
+      .pipe(first())
+      .subscribe(
+        data => {
+          if (data != null) {
+            this.statistics = Object.assign(new Statistics(), data);
+            this.statistics.standardDeviationAmount = Math.round((this.statistics.standardDeviationAmount + Number.EPSILON) * 100) / 100
+            this.router.navigate([this.returnUrl]);
+          }
+        },
+        error => {
+          this.alertService.error(error);
+          this.loading = false;
+        });
+  }
 }
