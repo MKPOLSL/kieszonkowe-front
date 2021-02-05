fa422b3464d89f6ae8dbbf7ff288dc7a178de275
paweryk807 <paweryk807@student.polsl.pl>
2021-02-05 16:15:02 +0100

    Update add-edit.component.ts

--- src/app/components/childs/add-edit.component.ts
+++ src/app/components/childs/add-edit.component.ts
@@ -39,21 +39,7 @@ export class AddEditComponent implements OnInit {
     ngOnInit() {
         this.id = this.route.snapshot.params['id'];
         this.isAddMode = !this.id;
-        
-        // password not required in edit mode
-        const passwordValidators = [Validators.minLength(6)];
-        if (this.isAddMode) {
-            passwordValidators.push(Validators.required);
-        }
-        
-
-        // this.form = this.formBuilder.group({
-        //     firstName: ['', Validators.required],
-        //     lastName: ['', Validators.required],
-        //     username: ['', Validators.required],
-        //     password: ['', passwordValidators]
-        // });
-
+                
         this.form = this.formBuilder.group({
             name: ['', Validators.required],
             education: ['', Validators.required],
@@ -92,26 +78,16 @@ export class AddEditComponent implements OnInit {
 
     }
 
-    // convenience getter for easy access to form fields
     get f() { return this.form.controls; }
 
     onSubmit() {
         this.submitted = true;
 
-        // reset alerts on submit
         this.alertService.clear();
 
-        // stop here if form is invalid
         if (this.form.invalid) {
             return;
         }
-
-        // this.loading = true;
-        // if (this.isAddMode) {
-        //     this.createUser();
-        // } else {
-        //     this.updateUser();
-        // }
 
         this.loading = true;
         if (this.isAddMode) {
@@ -120,20 +96,6 @@ export class AddEditComponent implements OnInit {
             this.updateUser();
         }
     }
-
-    // private createUser() {
-    //     this.accountService.register(this.form.value)
-    //         .pipe(first())
-    //         .subscribe(
-    //             data => {
-    //                 this.alertService.success('User added successfully', { keepAfterRouteChange: true });
-    //                 this.router.navigate(['.', { relativeTo: this.route }]);
-    //             },
-    //             error => {
-    //                 this.alertService.error(error);
-    //                 this.loading = false;
-    //             });
-    // }
 
     private updateUser() {
         this.accountService.update(this.id, this.form.value)
